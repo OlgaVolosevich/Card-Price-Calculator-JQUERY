ef11db138d8da25b41ee3f07ff86f81f7c3468dd
Olga Volosevich <olga.lucky.v@gmail.com>
2020-04-09 15:36:01 +0300

    feat: add card price calculator

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 $(document).ready(function (){
-    $('select').change(function(){
-        $selectedEdition = $('select#card_edition').val();
-        $selectedPaper = $('select#card_paper').val();
-        $selectedColor = $('select#card_color').val();
+
+        $('span#final_price').text(getCardPrice());
         $colorFormat = $('select#card_color option:selected').attr('data-color');
+
+        $('select').change(function(){
+        $colorFormat = $('select#card_color option:selected').attr('data-color');
+        $('span#final_price').text(getCardPrice());
         $('.card_images img').hide();
+        
         switch($colorFormat) {
             case '1':
                 $('.card_images img#card_1_0').show(); 
@@ -23,4 +26,14 @@ $(document).ready(function (){
                 break;
         }
     })
+
+    function getCardPrice () {
+        $selectedEdition = $('select#card_edition').val();
+        $finalEdition = $selectedEdition / 30;
+        $paperRatio = $('select#card_paper option:selected').attr('data-paper-ratio');
+        $printFile = $('select#card_color option:selected').attr('data-print-file');
+        $printStamp = $('select#card_color option:selected').attr('data-stamp');
+        $finalPrice = $finalEdition * $paperRatio + parseInt($printFile) + $finalEdition * $printStamp;
+        return $finalPrice;
+    }
 })
